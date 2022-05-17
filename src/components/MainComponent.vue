4738d35d0c48e3bb0c72db312c6a89de6a496b6a
FrancescoCataletto <catalettofrancesco@gmail.com>
2022-05-17 15:00:48 +0200

    draft of main structure

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,7 +1,9 @@
 <template>
-  <div>
+  <main>
+      <div class="container">
 
-  </div>
+      </div>
+  </main>
 </template>
 
 <script>
@@ -11,5 +13,14 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "../assets//style/global.scss";
 
+main{
+    background-color: rgb(30, 45, 59);
+    height: calc(100vh - 80px);
+    .container{
+        background-color: red;
+        height: calc(100vh - 80px);
+    }
+}
 </style>
