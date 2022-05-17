a347824e109b01af5b1ed0c5203462ff18a48512
FrancescoCataletto <catalettofrancesco@gmail.com>
2022-05-17 15:46:49 +0200

    draft of music card logic

---
+++ src/components/CardComponent.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+      <h2>{{musicCard.author}}</h2>
+      <h3>{{musicCard.genre}}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "CardComponent",
+    props:{
+        musicCard: Object
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+li{
+    color: red;
+}
+</style>
--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,14 +1,39 @@
 <template>
   <main>
       <div class="container">
-
+          <CardComponent v-for="(card, index) in musicArray" 
+                        :key="`card-${index}`"
+                        :musicCard="card"/>
       </div>
   </main>
 </template>
 
 <script>
+import CardComponent from './CardComponent.vue';
+import axios from "axios";
+
 export default {
-    name: "MainComponent"
+    name: "MainComponent",
+    components: { CardComponent },
+    data(){
+    return{
+      baseURL: "https://flynn.boolean.careers/exercises/api/array/music",
+      musicArray: []
+    }
+  },
+  methods: {
+    getAPI(){
+      axios.get(this.baseURL)
+      .then(res => {
+        this.musicArray = res.data.response;
+        console.log(res.data.response)
+      })
+    }
+  },
+
+  mounted(){
+    this.getAPI()
+  }
 }
 </script>
 
@@ -19,7 +44,6 @@ main{
     background-color: rgb(30, 45, 59);
     height: calc(100vh - 80px);
     .container{
-        background-color: red;
         height: calc(100vh - 80px);
     }
 }
