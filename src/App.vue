eef66559b6cd65285ba61b4012a52a3a1ed8bda6
FrancescoCataletto <catalettofrancesco@gmail.com>
2022-05-17 14:47:11 +0200

    header draft completed

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,29 @@
 <template>
-  <div >
-    CIAO
+  <div>
+    <HeaderComponent />
+
+    <MainComponent />
   </div>
 </template>
 
 <script>
+import HeaderComponent from './components/HeaderComponent.vue';
+import MainComponent from './components/MainComponent.vue';
 
 
 export default {
   name: 'App',
   components: {
-    
-  }
+    HeaderComponent,
+    MainComponent
+}
 }
 </script>
 
 <style lang="scss">
-
+*{
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
 </style>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,42 @@
+<template>
+  <header>
+      <div id="menu" class="container-fluid">
+          <div>
+              <a href="#"><img src="../assets/style/spotify-logo-png-7053.png" alt=""></a>
+          </div>
+          <div>
+              <select name="" id="">
+                  <option value="Choose" selected>Seleziona un genere</option>
+                  <option value="rock">Rock</option>
+                  <option value="Jazz">Jazz</option>
+                  <option value="Country">Country</option>
+                  <option value="Hip-Hop">Hip-Hop</option>
+              </select>
+          </div>
+      </div>
+  </header>
+</template>
+
+<script>
+export default {
+    name: "HeaderComponent"
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../assets//style/global.scss";
+
+header{
+    height: 80px;
+    background-color: rgb(41, 49, 58);
+}
+#menu{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 80px;
+    img{
+        height: 50px;
+    }
+}
+</style>
