60ed85ec858cdf6397634fb66d1a06b27064c7c1
FrancescoCataletto <catalettofrancesco@gmail.com>
2022-05-17 19:50:30 +0200

    created separate component for the loader animation

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,8 +1,7 @@
 <template>
   <main>
-      <div v-if="!isLoaded" id="loader">
-          <h2>LOADING YOUR MUSIC, PLEASE WAIT...</h2>
-          <div class="lds-ripple"><div></div><div></div></div>
+      <div v-if="!isLoaded">
+          <LoadingComponent />
       </div>
       <div class="container" v-else>
           <CardComponent v-for="(card, index) in musicArray" 
@@ -15,10 +14,11 @@
 <script>
 import CardComponent from './CardComponent.vue';
 import axios from "axios";
+import LoadingComponent from "./LoadingComponent.vue";
 
 export default {
     name: "MainComponent",
-    components: { CardComponent },
+    components: { CardComponent, LoadingComponent },
     data(){
     return{
       baseURL: "https://flynn.boolean.careers/exercises/api/array/music",
@@ -54,59 +54,6 @@ main{
         flex-wrap: wrap;
         height: calc(100vh - 80px);
     }
-}
-
-#loader{
-    h2{
-        color: white;
-    }
-}
-
-.lds-ripple {
-  display: inline-block;
-  position: relative;
-  width: 80px;
-  height: 80px;
-}
-.lds-ripple div {
-  position: absolute;
-  border: 4px solid #fed;
-  opacity: 1;
-  border-radius: 50%;
-  animation: lds-ripple 1s cubic-bezier(0, 0.2, 0.8, 1) infinite;
-}
-.lds-ripple div:nth-child(2) {
-  animation-delay: -0.5s;
-}
-@keyframes lds-ripple {
-  0% {
-    top: 36px;
-    left: 36px;
-    width: 0;
-    height: 0;
-    opacity: 0;
-  }
-  4.9% {
-    top: 36px;
-    left: 36px;
-    width: 0;
-    height: 0;
-    opacity: 0;
-  }
-  5% {
-    top: 36px;
-    left: 36px;
-    width: 0;
-    height: 0;
-    opacity: 1;
-  }
-  100% {
-    top: 0px;
-    left: 0px;
-    width: 72px;
-    height: 72px;
-    opacity: 0;
-  }
 }
 
 </style>
